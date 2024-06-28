26b25e7a48d8a799e4eec081690d1b55a07c96e9
ValeryA26 <andy21211309@gmail.com>
2024-06-27 22:52:02 -0500

    Inventario buscador

--- src/app/pages/inventario/inventario.component.ts
+++ src/app/pages/inventario/inventario.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { TabsComponent } from '../../core/components/tabs/tabs.component';
+import { FormsModule } from '@angular/forms';
+import { ClrIconModule, ClrInputModule } from '@clr/angular';
 
 @Component({
   selector: 'app-inventario',
   standalone: true,
-  imports: [TabsComponent],
+  imports: [TabsComponent, FormsModule, ClrInputModule, ClrIconModule],
   templateUrl: './inventario.component.html',
   styleUrl: './inventario.component.scss'
 })
 export class InventarioComponent {
-
+  input='';
 }
