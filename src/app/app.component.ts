9abb324a6f095bb20b521f28febbbf687e237706
ValeryA26 <andy21211309@gmail.com>
2024-10-27 07:43:14 -0500

    Inicio del menú de ventas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from './core/components/tabs/tabs.component';
+import '@cds/core/icon/register.js';
+import { ClarityIcons, userIcon } from '@clr/core/icon';
+
+ClarityIcons.addIcons(userIcon);
 
 @Component({
   selector: 'app-root',
--- src/app/pages/ventas/ventas.component.ts
+++ src/app/pages/ventas/ventas.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TabsComponent } from '../../core/components/tabs/tabs.component';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ClrFormsModule } from '@clr/angular';
+import '@cds/core/icon/register.js';
+import '@cds/core/button/register.js';
+import { ClarityIcons, userIcon } from '@cds/core/icon';
+import '@clr/icons';
+
+ClarityIcons.addIcons(userIcon);
 
 @Component({
   selector: 'app-ventas',
   standalone: true,
-  imports: [TabsComponent],
+  imports: [TabsComponent, ClrFormsModule, ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './ventas.component.html',
   styleUrl: './ventas.component.scss'
 })
+
+
+
 export class VentasComponent {
 
 }
